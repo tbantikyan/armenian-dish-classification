171a1e3b371aa2603d76d7024b518b889090c246
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-15 14:39:12 -0500

    finalized footer

--- sohamov/src/App.jsx
+++ sohamov/src/App.jsx
@@ -2,7 +2,7 @@ import { ref, getDownloadURL } from "firebase/storage";
 import { useState, useEffect } from 'react'
 import * as tf from "@tensorflow/tfjs";
 
-import DISH_CLASSSES from "./assets/dishClasses"; // dictionary with model's classes
+import DISH_CLASSSES from "./assets/dishClasses.js"; // dictionary with model's classes
 import firebaseStorage from "./api/firebaseconfig.js" // object for firebase storage reads
 import ImageUploader from "./components/ImageUploader"; // image uploading component
 import logo from "./assets/sohamov-logo.png" // site logo
@@ -91,19 +91,19 @@ function App() {
             <p className="info">
               So Hamov (hamov or համով meaning delicious in Armenian) is an image classification app that uses a trained 
               machine learning model to identify dishes popular in Armenian cuisine. Although many of these dishes are 
-              enjoyed in several cultures around the world, the model was trained to classify them because they are 
-              particularly staples of Armenian cuisine and culture.
+              found in cultures around the world, the model was trained to classify them because they are particularly 
+              staples of Armenian cuisine and culture.
               <br /><br />
               <span className="bold">Uploading an image</span><br/>
-              To provide the machine learning mdoel an image to classify, upload an image format file by clicking within 
-              the box on the left. You can ensure that the prediction is as accurate as possible by using a high quality 
+              To provide the machine learning model an image to classify, upload an image format file by clicking within 
+              the box on the left. You can ensure that the prediction is as accurate as possible by using a high-quality 
               image. Avoid low-resolution, blurry, and low-light photos. The dish you wish to identify should also be 
               clearly in frame, with as few additional subjects as possible.
               <br /><br />
               <span className="bold">Understanding the prediction</span><br/>
               Upon pressing predict, the model determines probabilities for each potential classification. If the model is
-              very confident, only one prediction will be shown. Otherwise, two or three prediction may be shown at once. 
-              The webapp will also display information about the model's most confident prediction. To view information 
+              very confident, only one prediction will be shown. Otherwise, two or three predictions may be shown at once. 
+              The web app will also display information about the model's most confident prediction. To view information 
               about a different prediction, you can click on the corresponding predicted dish name.
               <br /><br />
               <span className="lightBold">Note:</span> if none of the predictions are correct, then either:<br />
@@ -118,32 +118,33 @@ function App() {
                   <span className="dashed-item">basturma (բաստուրմա)</span><br/>
                 </span>
                 <span>
+                  <span className="dashed-item">harissa (հարիսա)</span><br/>
                   <span className="dashed-item">manti (մանթի)</span><br/>
-                  <span className="dashed-item">kufta (քուֆթա)</span><br/>
                   <span className="dashed-item">tolma (տոլմա)</span><br/>
                 </span>
                 <span>
                   <span className="dashed-item">spas (սպաս)</span><br/>
                   <span className="dashed-item">lahmajo (լահմաջո)</span><br/>
-                  <span className="dashed-item">harissa (հարիսա)</span><br/>
+                  <span className="dashed-item">kufta (քուֆթա)</span><br/>
                 </span>
               </span>
             </p>
             <h3 id="section-header">About the app</h3>
             <p className="info">
-              I got the idea to make this app after a few instances of friends reaching out to me with hopes that I could 
-              identify an Armenian dish they had eaten at a gathering or restaurant. Intriguied by machine learning and 
-              image analysis, I thought it'd be a fun project to create a webapp that could classify staples of Armenian
-              cuisine.<br/>
+              I got the idea to make this app after the third or fourth time a friend reached out to me with hopes that 
+              I could  identify an Armenian dish they had eaten at a gathering or restaurant. Already intrigued by machine 
+              learning and image analysis, I thought it'd be a fun project to create a web app that could classify popular 
+              Armenian cuisine.<br/>
               <br/>
-              I gathered a dataset of nearly one thousand images of dishes and trained the model with a success rate of roughly
-              98% in testing. I developed a functional and visually appealing webapp with React to allow anyone to 
-              play around with the model. Overall, the project was enjoyable and a wonderful learning experience. 
+              I gathered a dataset of nearly one thousand images of dishes and trained a model with a success rate of roughly
+              98% in testing. I developed a functional and visually appealing web app with React to allow anyone to play 
+              around with the model. Overall, the project was enjoyable and a wonderful learning experience. 
             </p>
           </div>
         </main>
         <footer>
-
+          <img id="footer-logo" src={logo} />
+          <p className="footer-text">Copyright @ 2023 Tigran Bantikyan</p>
         </footer>
       </>
     );
