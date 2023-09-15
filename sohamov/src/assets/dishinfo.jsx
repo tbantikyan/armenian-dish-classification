a659b92850d286e48b92ab82a571a10b499f7ee3
Tigran Bantikyan <tigranbantikyan@gmail.com>
2023-09-15 13:55:20 -0500

    Finished dish info

--- sohamov/src/assets/dishinfo.jsx
+++ sohamov/src/assets/dishinfo.jsx
@@ -2,12 +2,12 @@ const DISH_INFO = {
     'Բաստուրմա (basturma)': 
         <p className="dish-info">
             <span className="bold light">Basturma</span>, historically known as aboukh, is cured meat renowned for its
-            unqiue spice coating. Although typically prepared from beef, versions of the dish can be found across 
+            unique spice coating. Although typically prepared from beef, versions of the dish can be found across 
             different regions made with various meats and spices.<br/>
             <br/>
-            The dish is prepared by coating the meat with a mixture of garlic, paprica, fenugreek, and
+            The dish is prepared by coating the meat with a mixture of garlic, paprika, fenugreek, and
             other aromatic spices in a blend known as chaman (Armenian: <span className="italic">չաման</span>). The meat
-            is then hung to air-dry, during which the spice coating becomes a red shell.<br/>
+            is hung to air-dry, during which the spice coating becomes a red shell.<br/>
             <br/>
             Basturma is enjoyed in a variety of contexts. It can be paired with bread, cheese, and olives, served as a 
             part of sandwiches and wraps, or added to other dishes. Most commonly, the cured meat is cooked into an 
@@ -15,76 +15,118 @@ const DISH_INFO = {
         </p>,
     'Գաթա (gata)':
         <p className="dish-info">
-            <span className="bold light">Gata</span> is a delicious sweet treat made from rich, flaky dough and a sweet
+            <span className="bold light">Gata</span> is a delicious treat made from rich, flaky dough and a sweet
             filling. The pastry can come in a variety of forms, though it is typically shaped like a round cake or 
             smaller, individual loaves.<br/>
             <br/>
             Gata is prepared with a mixture of nuts, commonly walnuts or almonds, sugar, and butter. The mixture is rolled
-            into the dough such that it creates layers of filling within the pastry. The dough itself is kneaded so that 
-            is will be light and airy.<br/>
+            into the dough such that it creates layers of filling within the pastry. The dough is well-kneaded to ensure
+            the pastry is light and airy.<br/>
             <br/>
-            The pastry holds significance in Armenian culture and can be found decorated with designs pertaining to 
-            celebrations, village names, regional landmarks, or other intricate patterns. It is often baked for
-            Easter, Christmas, and New Years.
+            The pastry holds significance in Armenian culture and can be found decorated with designs of
+            village names, regional landmarks, celebrations, or other intricate patterns. It is often baked for
+            Easter, Christmas, and New Year.
         </p>,
     'Ղափամա (ghapama)':
         <p className="dish-info">
-            <span className="bold light">Ghapama</span> is a traditional Armenian dish that consists of a pumpkin stuffed
-            with rich, filling, yet sweet mixture. The dish is most often seen as the centerpiece of the table at special 
-            occasions, particularly for New Year's and Christmas festivities.<br/>
+            <span className="bold light">Ghapama</span> is a traditional Armenian dish that is made from a pumpkin 
+            stuffed with a rich yet sweet mixture. The dish is most often seen as the centerpiece of the 
+            table at special occasions, particularly for New Year and Christmas festivities.<br/>
             <br/>
-            Conventially, ghapama is prepared with a sugar pumpkin, though some may substitue with other varities of 
-            squash. The filling is prepared with rice, fresh nuts like walnuts, cashews, and pignolias, dried fruits,
-            butter, honey, and various warm spices.<br/>
+            Conventionally, ghapama is prepared with a sugar pumpkin, though some may substitute it with other
+            squash varieties. The filling is made with rice, fresh nuts like walnuts, cashews, and pignolias, 
+            dried fruits, butter, honey, and various warm spices.<br/>
             <br/>
-            The presentation of the ghapama is also given consideration, as many prefer to cut the lid of the pumpkin 
-            in a decorative pattern.
+            The presentation of the ghapama is also a point of consideration, as many like to cut the lid of the 
+            pumpkin in a decorative pattern.
         </p>,
     'Հարիսա (harissa)':
         <p className="dish-info">
-            <span className="bold light">Harissa</span> is a hearty porridge prepared from hulled wheat and chicken, 
-            and is popularly enjoyed during the winter months. The dish has deep significance in Armenian history 
-            and is consequently a national dish of country.<br/>
+            <span className="bold light">Harissa</span> is a hearty porridge prepared from hulled wheat and chicken
+            that is popularly enjoyed during the winter months. The dish has deep significance in Armenian history 
+            and is consequently a national dish of the country.<br/>
             <br/>
             While Harissa refers to a hot pepper paste in other parts of the world, Armenians use the name exclusively
-            for this dish. The main ingredients the grain and protein, which are simmered for hours alongside various
-            aromatic spices to create a thick porridge with tender meat.<br/>
+            for this dish. The main ingredients are a form of grain and protein, which are simmered for hours alongside 
+            various aromatic spices to create a thick porridge with tender meat.<br/>
             <br/>
             Harissa is celebrated in Armenia for sustaining the Armenians of Musa Ler who resisted the Turks and Kurds 
-            during the Armenian genocide. Every year, Armenians hold a Harissa day celebration. The dish is also 
+            during the Armenian genocide. Every year, Armenians hold a Harissa Day celebration. The dish is also 
             popular during Easter.
         </p>,
     'Քուֆթա (kufta)':
         <p className="dish-info">
-            <span className="bold light">Kufta</span> refers to a family of dishes, enjoyed in many cultures, that 
-            resemble oblong-shaped meatballs. As a results, the dish is prepared in many ways and is known by 
+            <span className="bold light">Kufta</span> refers to a family of dishes that resemble oblong-shaped 
+            meatballs. Since the dish is enjoyed in many cultures, it is prepared in many ways and known by 
             numerous names. Armenian steak tartare, for example, refers to kufta made from raw beef.<br/>
             <br/>
-            A traditional and particularly Armenian variant of kufta is vospov kufta (Armenian:
-            <span className="italic"> ոսպով քուֆթա</span>), which is vegeterian kufta made from red lentils 
+            A traditional and particularly Armenian variant of the dish is vospov kufta (Armenian:
+            <span className="italic"> ոսպով քուֆթա</span>), which is vegetarian kufta made from red lentils 
             instead of meat. A defining feature of kufta is the use of bulgur or hulled wheat in the patties, 
             which helps bind the meat and gives flavor.<br/>
             <br/>
-            The meat is well seasoned and enjoyed with onions, herbs, lemons, and other garnishes. It is also common 
-            to prepare a sauce in which the kufta can be dipped.
+            The meatballs are well seasoned and enjoyed with onions, herbs, lemons, and other garnishes.
+            Kufta is also commonly dipped into a sauce.
             <br/>
             <br/>
         </p>,
     'Լահմաջո (lahmajo)':
         <p className="dish-info">
-            <span className="bold light">Lahmajo</span>
+            <span className="bold light">Lahmajo</span>, also referred to as "lahmajun" and "lahmajoon", is a 
+            savory treat that is often likened to pizza.<br/>
+            <br/>
+            Lahmajo is prepared by topping paper-thin flatbread dough with a ground meat topping, typically beef
+            or lamb. The meat is well flavored with a medley of strong spices like garlic, cumin, and Aleppo pepper,
+            with tomato paste and fresh vegetables mixed in for added depth.<br/>
+            <br/>
+            The dish can be enjoyed as a snack, an appetizer, or even an entrée. In many parts of the world, it is
+            a popular street food. Many like to garnish lahmajo with lemon juice, fresh herbs, and even dollops of 
+            sauces.
         </p>,
     'Մանթի (manti)':
         <p className="dish-info">
-            <span className="bold light">Manti</span>
+            <span className="bold light">Manti</span> is a savory dish consisting of tiny dumplings covered in a 
+            yogurt-based sauce.<br/>
+            <br/>
+            The preparation of manti is labor intensive, as each dumpling's dough must be rolled out into a tiny 
+            square or circle. The dough is then filled with well-seasoned ground beef or lamb. The opposite ends
+            of the dough are pinched together to make a traditional, attractive presentation of the dumplings.
+            The cooking of manti varies across regions. While it is most often baked in Armenian cuisine, some
+            prefer to boil or steam the manti.<br/>
+            <br/>
+            Manti is eaten by topping it with a sauce made from yogurt, garlic, oil, and an assortment of fresh
+            herbs and vegetables.
         </p>,
     'Սպաս (spas)':
         <p className="dish-info">
-            <span className="bold light">Spas</span>
+            <span className="bold light">Spas</span> is a traditional Armenian yogurt-based soup. It is a popular
+            comfort food enjoyed year-round.<br/>
+            <br/>
+            The primary ingredients of spas are yogurt, hulled wheat, butter, and herbs, most commonly mint, 
+            dill, or cilantro.<br/>
+            <br/>
+            Although the recipe is simple, preparing spas is a long involved process. The cook must be attentive
+            at the pot, constantly stirring and ensuring the soup does not boil to prevent the curdling of the
+            yogurt.<br/>
+            <br/>
+            Spas can be enjoyed warm or cold. In some regions, the proportion of wheat to soup is lower, allowing
+            the soup to be drank from a cup like a drink. The grain used and the consistency of the soup can also 
+            vary.
         </p>,
     'Տոլմա (tolma)':
         <p className="dish-info">
-            <span className="bold light">Tolma</span>
+            <span className="bold light">Tolma</span>, also known as dolma, refers to a family of dishes made with
+            stuffing vegetables or leaves with a rice or meat filling. Common in many parts of the world, the dish
+            goes by many names and has endless variations.<br/>
+            <br/>
+            In Armenia, tolma is predominantly prepared with grape leaves. The leaves are wrapped around a mélange
+            of rice, beef, and herbs and boiled. It is typical to exclude meat or include nuts. Pasuts tolma 
+            (Armenian: <span className="italic">պասուց տոլմա</span>) is a variation that is prepared 
+            with cabbage leaves and stuffed with a lentil and chickpea filling.<br/>
+            <br/>
+            Tolma is often eaten with garnishes of lemon and fresh herbs. In Armenian cuisine, it is popular to
+            top the tolma with matsun skhtor (Armenian: <span className="italic">մածուն սխտոր</span>), 
+            a sauce made from mixing garlic into yogurt.
         </p>,
 }
 
